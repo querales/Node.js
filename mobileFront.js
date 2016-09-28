6ae5430733928288be91e201464b55ca4161caf4
gabriella <guara58@hotmail.com>
2016-09-28 10:56:28 -0700

    Update mobileFront.js

--- mobileFront.js
+++ mobileFront.js
@@ -5,8 +5,8 @@
 require("appdynamics").profile({
     controllerHostName:'dev.demo.appdynamics.com',
     controllerPort:'80',
-  //  accountName:'customer1_f16aea9b-844d-476d-92db-60f3acaa620d',
-    accountName:'customer1',
+  //  accountName:'',
+    accountName:'',
     accountAccessKey:'',
     applicationName: 'Movie Tickets Online_GQ',
     tierName: 'mobileUI_GQ',
